a4a69b140c39eb2829eb7efe2262ef192900fa9a
luscatorress <torreslucas000@gmail.com>
2026-05-19 00:44:39 -0300

    Adicionado funções de mesa (buscar mesa, atualizar mesa, deletar mesa)

--- app/queries/mesa_queries.py
+++ app/queries/mesa_queries.py
@@ -33,3 +33,47 @@ def criar_mesa(numero, status):
                 (numero, status)
             )
         return cursor.fetchone()
+    
+def buscar_mesa_por_id(id_mesa):
+    try:
+        with get_cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT
+                    m.id_mesa,
+                    m.status,
+                    m.numero
+                FROM mesa m
+                WHERE m.id_mesa = %s
+                """,
+                (id_mesa,),
+            )
+            return cursor.fetchone()
+    except Exception as e:
+        raise
+
+def atualizar_mesa(id_mesa, numero, status):
+    with get_cursor(commit=True) as cursor:
+        cursor.execute(
+            """
+            UPDATE mesa
+            SET status = %s, numero = %s
+            WHERE id_mesa = %s
+            RETURNING id_mesa, status, numero
+            """,
+            (status, numero, id_mesa),
+        )
+        return cursor.fetchone()
+
+
+def deletar_mesa(id_mesa):
+    with get_cursor(commit=True) as cursor:
+        cursor.execute(
+            """
+            DELETE FROM mesa
+            WHERE id_mesa = %s
+            RETURNING id_mesa
+            """,
+            (id_mesa),
+        )
+        return cursor.fetchone()
